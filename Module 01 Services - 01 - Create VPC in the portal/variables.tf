0a26ea84c5c6a6bc7a46d121be03f6ab99bb7676
Phuc Tran <86236803+minhphuc2k21@users.noreply.github.com>
2023-10-19 09:33:55 +0700

    Update variables.tf

--- Module 01 Services - 01 - Create VPC in the portal/variables.tf
+++ Module 01 Services - 01 - Create VPC in the portal/variables.tf
@@ -1,12 +1,12 @@
 variable "client_id" {
   type = string
-  default = "5b2901cd-7080-4512-9e6a-1d2be5167096" // Client ID được cung cấp khi hoàn thành việc khởi tạo Service Account
+  default = "XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXX" // Client ID được cung cấp khi hoàn thành việc khởi tạo Service Account
 }
 variable "client_secret" {
   type = string
-  default = "b7b3841e-0c56-4983-ad34-ea6f40dcd8ce" // Client secret được cung cấp khi hoàn thành việc khởi tạo Service Account
+  default = "XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXX" // Client secret được cung cấp khi hoàn thành việc khởi tạo Service Account
 }
 variable "project_id" {
   type    = string
-  default = "pro-38f98372-0515-4c61-99bc-e353addfc180" // Project ID nằm
+  default = "pro-XXXXXXX-XXXX-XXXX-XXXX" // Project ID nằm
 }
